c3d08ea7ad51278b9e641d12bda8761b985124e6
Jhonatan Gonzalez <jhesgodi@gmail.com>
2022-03-10 14:01:40 +1100

    feat: add variant $always

--- src/classnames.ts
+++ src/classnames.ts
@@ -4,7 +4,9 @@ import isString from "./is-string";
 type MaybeString = string | null | undefined;
 
 export type Variations = {
+  /** @deprecated·use·$always·instead */
   $all?: MaybeString | Variations;
+  $always?: MaybeString | Variations;
   $none?: MaybeString | Variations;
   $nil?: MaybeString | Variations;
   [key: string]: MaybeString | Variations;
@@ -16,9 +18,24 @@ export type Source = {
   [key: string]: any;
 };
 
+/** @deprecated·replaced by $always */
 const $all = "$all";
+const $always = "$always";
 const $none = "$none";
 const $nil = "$nil";
+
+/**
+ * check if prop is directive $always
+ * @deprated must remove after completing deprecation of $all directive
+ */
+const isAlways = (prop: string): boolean => prop === $all || prop === $always;
+
+/**
+ * get the value from directive $always or always
+ * @deprated must remove after completing deprecation of $all directive
+ */
+const getAlwaysVariation = (variations: Variations | string | null | undefined): MaybeString | Variations =>
+  variations?.[$always] || variations?.[$all];
 
 export function get(
   prop: string,
@@ -30,8 +47,8 @@ export function get(
   // for the provided prop, there is no variation, we should not continue
   if (isNil(valueVariations)) return null;
 
-  // this is a $all hook and if it is set to actual CSS classes (string), we should return value here.
-  if (prop === $all) {
+  // this is a $always hook and if it is set to actual CSS classes (string), we should return value here.
+  if (isAlways(prop)) {
     if (isString(valueVariations) || isNil(valueVariations)) return valueVariations;
 
     return Object.keys(valueVariations)
@@ -58,33 +75,33 @@ export function get(
   // if value of the prop doesn't have a match, we should divert to $none hook here.
   const matchingVariations = valueVariations[correspondingSourceValue.toString()] || valueVariations[$none];
 
+  const alwaysVar = getAlwaysVariation(valueVariations);
+
   // this means we're either prop -> null / string so we don't need to dig more
   if (isNil(matchingVariations) || isString(matchingVariations)) {
     // prop value (or $none) doesn't map to any variation
-    // return value of $all hook, when it's available.
+    // return value of $always hook, when it's available.
     if (isNil(matchingVariations)) {
-      const allVar = valueVariations[$all];
+      // if $always hook maps to string/null, just return it. no need to dig more
+      if (isString(alwaysVar) || isNil(alwaysVar)) return alwaysVar;
 
-      // if $all hook maps to string/null, just return it. no need to dig more
-      if (isString(allVar) || isNil(allVar)) return allVar;
-
-      // $all maps to an object we should dig more
-      return Object.keys(allVar)
-        .map((nestedProp) => get(nestedProp, allVar[nestedProp], source))
+      // $always maps to an object we should dig more
+      return Object.keys(alwaysVar)
+        .map((nestedProp) => get(nestedProp, alwaysVar[nestedProp], source))
         .filter((v) => !isNil(v))
         .join(" ");
     }
 
-    // prop value (or $none) maps to string, if we don't have $all just return what we got
-    if (isNil(valueVariations.$all)) return matchingVariations;
+    // prop value (or $none) maps to string, if we don't have $always just return what we got
+    if (isNil(alwaysVar)) return matchingVariations;
 
-    // prop value (or $none) maps to string, we also got the $all hook, combine and return
-    return `${valueVariations.$all} ${matchingVariations}`;
+    // prop value (or $none) maps to string, we also got the $always hook, combine and return
+    return `${alwaysVar} ${matchingVariations}`;
   }
 
   return Object.keys(matchingVariations)
     .map((nestedProp) => get(nestedProp, matchingVariations[nestedProp], source))
-    .concat(isString(valueVariations.$all) ? valueVariations.$all : null)
+    .concat(isString(alwaysVar) ? alwaysVar : null)
     .filter((v) => !isNil(v))
     .join(" ");
 }
@@ -92,12 +109,16 @@ export function get(
 function classnames(variations: Variations, ...inputs: Array<Source | null | undefined>): string {
   const sources = inputs.filter((source) => !isNil(source)) as Source[];
 
+  const alwaysVar = getAlwaysVariation(variations);
   const vars = Object.keys(variations).map((prop) => {
-    // if (prop === $all) return variations[$all];
-    if (prop === $all && (isString(variations[$all]) || isNil(variations[$all]))) return variations[$all];
+    if (isAlways(prop) && (isString(alwaysVar) || isNil(alwaysVar))) {
+      return alwaysVar;
+    }
 
     const variation = variations[prop];
-    if (isNil(variation)) return null;
+    if (isNil(variation)) {
+      return null;
+    }
 
     return sources.map((source) => get(prop, variation, source)).filter((v) => !isNil(v));
   });
